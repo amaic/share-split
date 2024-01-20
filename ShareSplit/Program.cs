4ee66f5620513a5ca133297510c093873dd8f3b7
Alexander Mauri <maurialex@web.de>
2024-01-20 07:18:27 +0100

    foundation

--- ShareSplit/Program.cs
+++ ShareSplit/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var hostBuilder = Host.CreateApplicationBuilder();
+
+var host = hostBuilder.Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Starting host.");
+
+host.Run();
+
+logger.LogInformation("Host stopped.");
